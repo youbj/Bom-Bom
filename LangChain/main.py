b450d704b45311989606123267d4cf7b90292fca
유병주 <dbqudwn011@naver.com>
2024-11-15 13:44:30 +0900

    FIX: 배포를 위해 reload=False로 수정, 로컬에서 테스트원하면 main.py에서 수정 필요

--- LangChain/main.py
+++ LangChain/main.py
@@ -48,4 +48,4 @@ def get_gpt_service():
     return gpt_service
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=False)
